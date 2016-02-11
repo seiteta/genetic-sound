d93e719b81020b6148cd1a072b9fe2170b059f34
Frédéric Bardolle <Fred@Plavalaguna.local>
2016-02-11 18:00:21 +0100

    First .wav file creation ♪

---
+++ wave_creator.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 17:56:46 2016
+
+@author: Fred
+"""
+
+
+# Import librairies
+import numpy as np
+from scipy.io.wavfile import write
+
+# Generate a random array (44100 random samples between -1 and 1)
+data = np.random.uniform(-1,1,44100)
+
+# No idea what this line does...
+scaled = np.int16(data/np.max(np.abs(data)) * 32767)
+
+# Write the .wav file
+write('test.wav', 44100, scaled)
